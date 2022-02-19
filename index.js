31ec302b4584b7fed24c8cc5ddea5d99f6174f55
Luz <aracely.des@uanl.edu.mx>
2022-02-19 10:51:38 -0600

    JS

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 console.log("hola mundo!");
-//Paso 1: Calcular total
 
+//Declarar variables
 var total = 0;
 var monto = 0;
 
+//Asignar elementos del documento a variables
 var montoTxt = document.getElementById("monto");
-
 var totalMsg= document.getElementById("totalMsg");
+var descripcionTxt = document.getElementById("descripcion");
+var listaTicket = document.getElementById("ticket");
 
-
+//Calcular el total
 function calcularTotal() {
     //obtener el monto del campo de texto
     monto = montoTxt.value;
@@ -16,7 +18,28 @@ function calcularTotal() {
     
     total = total + monto;
    
-
-    //Imprimir en pagina
+    //Imprimir Total en pagina
     totalMsg.innerHTML = "Total: " + total;
+
+    imprimirTicket(monto);
+}
+
+function imprimirTicket(monto) {
+
+//Crear elementos de lista
+     var elementoTicket = document.createElement("ion-item"); //elemento de lista
+     var labelElemento = document.createElement("ion-label"); //etiqueta de elemento
+
+//Crear el texto para la etiqueta
+     labelElemento.innerHTML = descripcionTxt.value + " : $" + monto;
+//Asignar la etiqueta en el elemento de la lista
+     elementoTicket.appendChild(labelElemento);
+
+//Inyectar el elemento creado en la lista 
+     listaTicket.appendChild(elementoTicket);
+
+//Limpiar campos de texto
+    descripcionTxt.value = "";
+    montoTxt.value = "";
+
 }
